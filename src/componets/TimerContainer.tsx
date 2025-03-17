cb51372085f60d5074dc942dd05be6e334935a35
JoakinGC <joakingcontreras2@gmail.com>
2025-03-17 13:13:17 +0100

    feat:media query button

--- src/componets/TimerContainer.tsx
+++ src/componets/TimerContainer.tsx
@@ -13,31 +13,37 @@ const TimerContainer = ({timerProps}:{timerProps:FormTimerProps}) =>{
     const { hours, minutes, seconds } = toHms(timerState.currentTime);
 
     return(
+        <>
+        <article>
+                <h2>Estás en:</h2>
+                <h3>
+                    {(timerState.isFinally) ? "Fin": timerState.isBreak ?"Descanso" :"Round"}
+                </h3>
+            </article>
         <article className="timer-container">
-            <h2>Estás en:</h2>
-            <h3>
-                {(timerState.isFinally) ? "Fin": timerState.isBreak ?"Descanso" :"Round"}
-            </h3>
-            <Timer
-                hours={hours}
-                minutes={minutes}
-                seconds={seconds}
-            />
-            <hr/>
             <article>
-                <article>
-                    <h2>Número de Rounds</h2>
-                    <p>{timerState.numberRounds}</p>
-                </article>
-                <article>
-                    <h2>Rounds completos:</h2>
-                    <p>{timerState.roundsRemaining}</p>
-                </article>
-                <button disabled={timerState.buttonActive.buttonStop} onClick={() => stopTimer()}>Parar</button>
-                <button disabled={timerState.buttonActive.buttonInit} onClick={() => startTimer()}>Iniciar</button>
-                <button disabled={timerState.buttonActive.buttonRestart} onClick={() => resetTimer()}>Reinciar</button>
+                <h2>Número de Rounds</h2>
+                <p>{timerState.numberRounds}</p>
+            </article>
+            <article>
+                <Timer
+                    hours={hours}
+                    minutes={minutes}
+                    seconds={seconds}
+                />
+            </article>
+            <article>
+                <h2>Rounds completos:</h2>
+                <p>{timerState.roundsRemaining}</p>
             </article>
         </article>
+        <article>
+            <button disabled={timerState.buttonActive.buttonStop} onClick={() => stopTimer()}>Parar</button>
+            <button disabled={timerState.buttonActive.buttonInit} onClick={() => startTimer()}>Iniciar</button>
+            <button disabled={timerState.buttonActive.buttonRestart} onClick={() => resetTimer()}>Reinciar</button>
+        </article>
+        </>
+
     )
 }
 
